c88e6846e56e9ac12fb49af878d0fd6a96624680
FrankLovelace <alexandrogallegos12@gmail.com>
2026-01-13 20:53:38 -0600

    input logica de colision logica numerica basica y regla de dstruccion

--- probador/obj/Debug/net8.0/probador.AssemblyInfo.cs
+++ probador/obj/Debug/net8.0/probador.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("probador")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbbdb3251393c0a8572391e9647069cddc5210a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f47b9eb2ebf9b1bf13b6cdfa7d7baa2d82919a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("probador")]
 [assembly: System.Reflection.AssemblyTitleAttribute("probador")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
